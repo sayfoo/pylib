5530ae627a769bac907559fc426aa8fa6594e375
sayfoo <kim.sajoong@gmail.com>
2022-05-17 16:11:02 +0900

    re_sample.py update

--- ch01/re_sample.py
+++ ch01/re_sample.py
@@ -1,10 +1,12 @@
 import re
+from typing import Pattern
 
 data = """
 홍길동의 주민번호는 800905-1049118 입니다. 
-그리고 고길동의 주민번호는 700905-1059119 입니다.
+그리고 고길동의 주민번호는 700905-0059119 입니다.
 그렇다면 누가 형님일까요?
 """
 
-pat = re.compile("(\d{6})[-]\d{7}")
-print(pat.sub("\g<1>-*******", data))
+pat = re.compile("(\d{6})[-](\d{1})\d{6}")
+print(pat.sub("\g<1>-\g<2>******", data))
+
